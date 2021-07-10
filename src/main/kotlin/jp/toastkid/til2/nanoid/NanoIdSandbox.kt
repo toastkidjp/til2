37288848eca7b83a38dad0d31021ffa0f69725b5
Toast Kid <super_q1m@yahoo.co.jp>
2021-07-11 08:41:52 +0900

    feature Add charArray case.

--- src/main/kotlin/jp/toastkid/til2/nanoid/NanoIdSandbox.kt
+++ src/main/kotlin/jp/toastkid/til2/nanoid/NanoIdSandbox.kt
@@ -1,7 +1,9 @@
 package jp.toastkid.til2.nanoid
 
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils
+import java.security.SecureRandom
 
 fun main() {
-    repeat(100) { println(NanoIdUtils.randomNanoId()) }
+    repeat(10) { println(NanoIdUtils.randomNanoId()) }
+    repeat(10) { println(NanoIdUtils.randomNanoId(SecureRandom(), charArrayOf('a', 'x', '2', '$', '@'), 12)) }
 }
