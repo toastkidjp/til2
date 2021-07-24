05443fc541f941f7d6ac40f6b1880c6404907ca6
Toast Kid <super_q1m@yahoo.co.jp>
2021-07-25 07:21:18 +0900

    refactor Divide to 2 functions.

--- src/main/kotlin/jp/toastkid/til2/nanoid/NanoIdSandbox.kt
+++ src/main/kotlin/jp/toastkid/til2/nanoid/NanoIdSandbox.kt
@@ -4,6 +4,13 @@ import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 import java.security.SecureRandom
 
 fun main() {
+    generateNanoId()
+}
+
+private fun generateNanoId() {
     repeat(10) { println(NanoIdUtils.randomNanoId()) }
+}
+
+private fun generateRestrictedNanoId() {
     repeat(10) { println(NanoIdUtils.randomNanoId(SecureRandom(), charArrayOf('a', 'x', '2', '$', '@'), 12)) }
 }
